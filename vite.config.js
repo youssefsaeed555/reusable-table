86b1e141df3cec754b61486be7db77ce4d435cdc
youssef saeed <yossefsaid555@gmail.com>
2025-08-01 22:28:59 +0300

    feat: Add path alias resolution in Vite configuration for improved module imports

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+      components: path.resolve(__dirname, "src/components"),
+      constants: path.resolve(__dirname, "src/constants"),
+      utils: path.resolve(__dirname, "src/utils"),
+      styles: path.resolve(__dirname, "src/styles"),
+      assets: path.resolve(__dirname, "src/assets"),
+    },
+  },
+});
