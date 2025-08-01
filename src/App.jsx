b8d9301cb514da33cb698586c665aa73ff423bf3
youssef saeed <yossefsaid555@gmail.com>
2025-08-01 22:28:47 +0300

    feat: Implement App component layout with Analytics integration

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
+import Analytics from "./components/Analytics/analytics";
+
+import "./styles/app.less";
+
 function App() {
-  return <></>;
+  return (
+    <main className="main-container">
+      <Analytics />
+    </main>
+  );
 }
 
 export default App;
