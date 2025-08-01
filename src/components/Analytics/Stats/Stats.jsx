74a558d5caad097115b9110dbe3b2ca4b299f4d4
youssef saeed <yossefsaid555@gmail.com>
2025-08-01 22:28:32 +0300

    feat: Add Stats component with layout, styling, and dummy data for analytics overview

---
+++ src/components/Analytics/Stats/Stats.jsx
@@ -0,0 +1,39 @@
+import { analyticsMapperIcon, formatCurrency } from "@/utils/analytics";
+import { ANALYSIS_DUMMY_STATS } from "@/constants/analytics";
+
+import "./Stats.less";
+
+const Stats = () => {
+  return (
+    <div className="analytics-section__stats">
+      {ANALYSIS_DUMMY_STATS.map((state) => {
+        const { title, value, change, id } = state;
+        const Icon = analyticsMapperIcon(id);
+
+        return (
+          <div
+            key={id}
+            className={`analytics-section__stats-item analytics-section__stats-item--${id}`}
+          >
+            <span className="analytics-section__stats-item-icon">
+              <Icon />
+            </span>
+            <section className="analytics-section__stats-item-content">
+              <h3 className="analytics-section__stats-item-content-value">
+                {formatCurrency({ amount_cents: value })}
+              </h3>
+              <span className="analytics-section__stats-item-content-title">
+                {title}
+              </span>
+              <span className="analytics-section__stats-item-content-change">
+                {change} in this month
+              </span>
+            </section>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Stats;
